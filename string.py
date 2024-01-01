70dabb2e41febff1ac7c00aa0a8462bd0e455140
nazaninabaslou <151542366+nazaninabaslou@users.noreply.github.com>
2024-01-01 18:07:09 +0330

    homework2

---
+++ string.py
@@ -0,0 +1,12 @@
+name="N a z a n i n "
+print(name)
+print(name[0])
+print(name[0:2])
+print(name.upper())
+print(name.lower())
+print(name.strip())
+print(name.lower().replace("n","s"))S
+print(name.replace(" ",""))
+print(name.lower().strip().replace(" ",""))
+print(len(name.strip().replace(" ", ""))/2)
+print(len(name.strip().replace(" ", ""))/2)S
